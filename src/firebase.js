1c46073d846126ce828a81a3c1c76b50e2b7e4f6
Herman Cai <hermancai3@gmail.com>
2022-01-16 02:09:13 -0800

    Update firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-const { initializeAppCheck, ReCaptchaV3Provider } = require("firebase/app-check");
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 const app = initializeApp({
-  apiKey: "AIzaSyCB7jMpAYp5yY0KGzcZuyHXTpi0_Y5dboo",
-  authDomain: "slack-clone-893d9.firebaseapp.com",
-  projectId: "slack-clone-893d9",
-  storageBucket: "slack-clone-893d9.appspot.com",
-  messagingSenderId: "639307756911",
-  appId: "1:639307756911:web:895615f64658d6ee1a649a",
+  apiKey: "AIzaSyAzoiHjsFj9RETsoYvRz_fo9nELKO_5kO8",
+  authDomain: "chatapp-e7cdc.firebaseapp.com",
+  projectId: "chatapp-e7cdc",
+  storageBucket: "chatapp-e7cdc.appspot.com",
+  messagingSenderId: "1750644893",
+  appId: "1:1750644893:web:82613f06448bf8c6d93c74",
 });
 
 initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider("6LcCEewdAAAAALp4evA_Zu0WNNsGtkPowCpEom3U"),
+  provider: new ReCaptchaV3Provider("6LfGQxceAAAAABZ5zNv1BtOWIjYBxizirUuNf4jR"),
   isTokenAutoRefreshEnabled: true,
 });
 
